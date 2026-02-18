2837185737b7e50a0552fe33db87a17d5d63a0af
tmyimai <89573801+tmyimai@users.noreply.github.com>
2026-02-18 11:53:10 +0900

    Update stackNsort.sh

--- Scripts/stackNsort.sh
+++ Scripts/stackNsort.sh
@@ -1,4 +1,4 @@
 # compile the files by seqkit
-cat *.fas > 20200808.fasta # taxus_name.fas files (e.g. Cryptomeria.fasta) are stacked into one fasta file
+cat *.fas > 20200808.fasta # taxus_name.fas files (e.g. Cryptomeria.fas) are stacked into one fasta file
 seqkit grep -nrip "matK" *.fas > 20200808matK.fasta # matK sequence was extracted from the stacked fasta file
 seqkit grep -nrip "rbcL" *.fas > 20200808rbcL.fasta # rbcL sequence was extracted from the stacked fasta file
